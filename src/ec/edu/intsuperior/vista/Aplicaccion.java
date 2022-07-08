50863ffad6e6b6b98760eddc824e5d8bbb40124e
jeffe <jeffe@192.168.56.1>
2022-07-08 00:46:31 -0500

    Prueba 1.1 del int POO

---
+++ src/ec/edu/intsuperior/vista/Aplicaccion.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Clase2;
+import ec.edu.intsuperior.modelo.Clase3;
+
+import ec.edu.intsuperior.modelo.Clase4;
+import ec.edu.intsuperior.modelo.Clase5;
+import ec.edu.intsuperior.modelo.ClaseJava;
+
+
+/**
+ *
+ * @author jeffe
+ */
+public class Aplicaccion {
+    public static void main(String[] args) {
+        ClaseJava obj=new ClaseJava();
+        System.out.println("Clase1 :Materias Lenhuaje Int ");
+        obj.MetodoinstanciaLengua1();
+        ClaseJava.MetodoTeoria1();
+        obj.MetodoinstanciaLengua2();
+        ClaseJava.MetodoTeorioa2();
+        obj.MetodoinstanciaLengua3();
+        ClaseJava.MetodoTeoria3();
+        obj.MetodoinstanciaLengua4();
+        ClaseJava.MetodoTeoria4();
+        obj.MetodoinstanciaLengua5();
+        ClaseJava.MetodoTeoria5();
+        System.out.println("............");
+    
+        System.out.println("Clase2: Marcas de carros del Int ");
+    Clase2 leer=new Clase2();
+    
+    leer.Carro6();
+    Clase2.Pro6();
+    leer.Carro7();
+    Clase2.Pro7();
+    leer.Carro8();
+    Clase2.Pro8();
+    leer.Carro9();
+    Clase2.Pro9();
+    leer.Carro10();
+    Clase2.Pro10();
+     
+        System.out.println("...............");
+        
+        System.out.println("Clase3: Materias Int");
+    Clase3 sj=new Clase3();
+    sj.Metodologia11();
+    Clase3.Teoria11();
+    sj.Metodologia12();
+    Clase3.Teorioa12();
+    sj.Metodologia13();
+    Clase3.Teoria13();
+    sj.Metodologia14();
+    Clase3.Teoria14();
+    sj.Metodologia15();
+    Clase3.Teoria15();
+        System.out.println("...............");
+        System.out.println("Clase4: Dispensas ganadoras del Int son: ");
+    Clase4 sc=new Clase4();
+    
+    sc.Ganador16();
+    Clase4.Nombre16();
+    sc.Ganador17();
+    Clase4.Nombre17();
+    sc.Ganador18();
+    Clase4.Nombre18();
+    sc.Ganador19();
+    Clase4.Nombre19();
+    sc.Ganador20();
+    Clase4.Nombre20();
+    
+    
+        System.out.println(".................");
+        System.out.println("Clase5 :Materias Poo Int");
+    Clase5 sn=new Clase5();
+    sn.Poo21();
+    Clase5.Teoria21();
+    sn.Poo22();
+    Clase5.Teorioa22();
+    sn.Poo23();
+    Clase5.Teoria23();
+    sn.Poo24();
+    Clase5.Teoria24();
+    sn.Poo25();
+    Clase5.Teoria25();
+    }
+}
